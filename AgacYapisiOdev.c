259e9539efef1d4f52f445c59adef0f24bb69721
Nevra Nur Doğan <122152162+NevraNurDogan@users.noreply.github.com>
2023-06-02 21:00:38 +0300

    Add files via upload

---
+++ AgacYapisiOdev.c
@@ -0,0 +1,191 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Ogrenci {
+    int ogrenciNumarasi;
+    char ad[50];
+    int vizeNotu;
+    int finalNotu;
+    float ortalama;
+    struct Ogrenci* sol;
+    struct Ogrenci* sag;
+};
+
+struct Ogrenci* dugumOlustur(int numara, const char* ad, int vize, int final) {
+    struct Ogrenci* yeniDugum = (struct Ogrenci*)malloc(sizeof(struct Ogrenci));
+    yeniDugum->ogrenciNumarasi = numara;
+    strcpy(yeniDugum->ad, ad);
+    yeniDugum->vizeNotu = vize;
+    yeniDugum->finalNotu = final;
+    yeniDugum->ortalama = (0.4 * vize) + (0.6 * final);
+    yeniDugum->sol = NULL;
+    yeniDugum->sag = NULL;
+    return yeniDugum;
+}
+
+struct Ogrenci* ekleAgaca(struct Ogrenci* kok, struct Ogrenci* yeniDugum) {
+    if (kok == NULL) {
+        return yeniDugum;
+    } else {
+        if (yeniDugum->ogrenciNumarasi <= kok->ogrenciNumarasi) {
+            kok->sol = ekleAgaca(kok->sol, yeniDugum);
+        } else {
+            kok->sag = ekleAgaca(kok->sag, yeniDugum);
+        }
+        return kok;
+    }
+}
+
+void inorderListele(struct Ogrenci* dugum) {
+    if (dugum != NULL) {
+        inorderListele(dugum->sol);
+        printf("%d %s %d %d %.2f\n", dugum->ogrenciNumarasi, dugum->ad, dugum->vizeNotu, dugum->finalNotu, dugum->ortalama);
+        inorderListele(dugum->sag);
+    }
+}
+
+struct Ogrenci* ara(struct Ogrenci* kok, int numara) {
+    if (kok == NULL || kok->ogrenciNumarasi == numara) {
+        return kok;
+    }
+    if (numara < kok->ogrenciNumarasi) {
+        return ara(kok->sol, numara);
+    }
+    return ara(kok->sag, numara);
+}
+
+struct Ogrenci* maxDugumBul(struct Ogrenci* kok) {
+    if (kok == NULL || kok->sag == NULL) {
+        return kok;
+    }
+    return maxDugumBul(kok->sag);
+}
+
+struct Ogrenci* sil(struct Ogrenci* kok, int numara) {
+    if (kok == NULL) {
+        return kok;
+    }
+    if (numara < kok->ogrenciNumarasi) {
+        kok->sol = sil(kok->sol, numara);
+    } else if (numara > kok->ogrenciNumarasi) {
+        kok->sag = sil(kok->sag, numara);
+    } else {
+        if (kok->sol == NULL) {
+            struct Ogrenci* temp = kok->sag;
+            free(kok);
+            return temp;
+        } else if (kok->sag == NULL) {
+            struct Ogrenci* temp = kok->sol;
+            free(kok);
+            return temp;
+        }
+        struct Ogrenci* temp = maxDugumBul(kok->sol);
+        kok->ogrenciNumarasi = temp->ogrenciNumarasi;
+        strcpy(kok->ad, temp->ad);
+        kok->vizeNotu = temp->vizeNotu;
+        kok->finalNotu = temp->finalNotu;
+        kok->ortalama = temp->ortalama;
+        kok->sol = sil(kok->sol, temp->ogrenciNumarasi);
+    }
+    return kok;
+}
+
+void menu() {
+    printf("----- MENU -----\n");
+    printf("1) AgacOlustur\n");
+    printf("2) Listele\n");
+    printf("3) Guncelle\n");
+    printf("4) Sil\n");
+    printf("0) Cikis\n");
+    printf("----------------\n");
+}
+
+int main() {
+    struct Ogrenci* kok = NULL;
+    FILE* dosya = fopen("C:\\Users\\Casper\\OneDrive\\Masaüstü\\öðrenci.txt", "r");
+    if (dosya == NULL) {
+        printf("Dosya acilamadi.\n");
+        return 1;
+    }
+
+    int ogrenciNumarasi, vizeNotu, finalNotu;
+    char ad[50];
+    float ortalama;
+
+    while (fscanf(dosya, "%d %s %d %d", &ogrenciNumarasi, ad, &vizeNotu, &finalNotu) != EOF) {
+        struct Ogrenci* yeniDugum = dugumOlustur(ogrenciNumarasi, ad, vizeNotu, finalNotu);
+        kok = ekleAgaca(kok, yeniDugum);
+    }
+
+    fclose(dosya);
+
+    int secim;
+    do {
+        menu();
+        printf("Seciminizi yapin: ");
+        scanf("%d", &secim);
+        switch (secim) {
+            case 1: {
+                int numara;
+                char ad[50];
+                int vize;
+                int final;
+                printf("Ogrenci numarasini girin: ");
+                scanf("%d", &numara);
+                printf("Adi girin: ");
+                scanf("%s", ad);
+                printf("Vize notunu girin: ");
+                scanf("%d", &vize);
+                printf("Final notunu girin: ");
+                scanf("%d", &final);
+                struct Ogrenci* yeniDugum = dugumOlustur(numara, ad, vize, final);
+                kok = ekleAgaca(kok, yeniDugum);
+                break;
+            }
+            case 2: {
+                printf("Ogrenciler:\n");
+                inorderListele(kok);
+                break;
+            }
+            case 3: {
+                int numara;
+                printf("Guncellenecek ogrencinin numarasini girin: ");
+                scanf("%d", &numara);
+                struct Ogrenci* ogrenci = ara(kok, numara);
+                if (ogrenci == NULL) {
+                    printf("Ogrenci bulunamadi.\n");
+                    break;
+                }
+                int yeniVize, yeniFinal;
+                printf("Yeni vize notunu girin: ");
+                scanf("%d", &yeniVize);
+                printf("Yeni final notunu girin: ");
+                scanf("%d", &yeniFinal);
+                ogrenci->vizeNotu = yeniVize;
+                ogrenci->finalNotu = yeniFinal;
+                ogrenci->ortalama = (0.4 * yeniVize) + (0.6 * yeniFinal);
+                break;
+            }
+            case 4: {
+                int numara;
+                printf("Silinecek ogrencinin numarasini girin: ");
+                scanf("%d", &numara);
+                kok = sil(kok, numara);
+                break;
+            }
+            case 0:
+                printf("Programdan cikiliyor.\n");
+                break;
+            default:
+                printf("Gecersiz secim. Lutfen tekrar deneyin.\n");
+                break;
+        }
+    } while (secim != 0);
+
+    return 0;
+}
+
+
+
